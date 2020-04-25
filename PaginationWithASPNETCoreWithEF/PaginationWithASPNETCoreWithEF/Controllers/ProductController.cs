47ac7cd42a0f79ac14b0d8058d7d1df2fc9c0e80
ozhangulal <ozhangulal@outlook.com>
2020-04-25 22:20:34 +0300

    PaginationMvcCoreAndEFCore
    using with pagedList tool and easy pagination example on MVCCore and EFCore

---
+++ PaginationWithASPNETCoreWithEF/PaginationWithASPNETCoreWithEF/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PagedList.Core;
+using PaginationWithASPNETCoreWithEF.Models;
+
+namespace PaginationWithASPNETCoreWithEF.Controllers
+{
+    [Route("Product")]
+    public class ProductController : Controller
+    {
+        private DataContext _db = new DataContext();
+
+        [Route("index")]
+        [Route("")]
+        [Route("~/")]
+        public IActionResult Index(int page = 1, int pageSize = 3)
+        {
+            PagedList<Product> model = new PagedList<Product>(_db.Product,page,pageSize);
+            return View("Index",model);
+        }
+    }
+}
---
+++ PaginationWithASPNETCoreWithEF/PaginationWithASPNETCoreWithEF/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaginationWithASPNETCoreWithEF.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        //public decimal UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
